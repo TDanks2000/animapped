ec2078aea5473470045939137b4e8b18ee2c9dd1
Tommy <tommydanks2000@outlook.com>
2023-10-10 07:43:03 +0100

    change cron job to setInterval

--- src/mappings/main.ts
+++ src/mappings/main.ts
@@ -1,22 +1,26 @@
 import { Mapping as MappingClass } from "./mapping";
 import Console from "@tdanks2000/fancyconsolelog";
 import { StateManager } from "./utils";
-import cron from "node-cron";
+
+import ms from "ms";
 
 const c = new Console();
 export class Mapping {
   private stateManager: StateManager = new StateManager();
-
-  constructor() {}
+  private timeBetweenMappings = ms("5 days");
 
   async startMapping() {
     const mapping = await MappingClass.create(this.stateManager);
 
-    cron.schedule("0 0 * * MON", async () => {
+    setInterval(async () => {
       if (this.stateManager.running === true) return;
-      c.info("it is now a monday, going through the list again");
+      c.info(
+        `it is has been ${ms(this.timeBetweenMappings, {
+          long: true,
+        })}, going through the list again`
+      );
 
       await mapping.start();
-    });
+    }, this.timeBetweenMappings);
   }
 }
